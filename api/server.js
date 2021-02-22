60ccbb8f4f62247ac0b0817a96c97bcbf9f899db
matieuruth <matieu.ruth@gmail.com>
2021-02-21 21:41:11 -0500

    testing codegrade

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const server = express();
 
 // Complete your server here!
+//added a comment to check
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
