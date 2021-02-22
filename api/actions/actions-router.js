b6834b9cacfdbbb345288b3bd676aaef6562a813
matieuruth <matieu.ruth@gmail.com>
2021-02-21 22:12:04 -0500

    structure set up

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -4,6 +4,28 @@ const actions = require('./actions-model');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to Sprint Actions'
+    })
+});
+
+router.get('/api/actions' (req, res, next) => {
+
+});
+
+router.get('/api/actions/:id' (req, res, next) => {
+
+});
+
+router.post('/api/actions' (req, res, next) => {
+
+});
+
+router.put('/api/actions/:id' (req, res, next) => {
+
+});
+
+router.delete('/api/actions/:id' (req, res, next) => {
 
 });
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,31 @@
 // Write your "projects" router here!
+const express = require('express');
+const actions = require('./projects-model');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to Sprint Projects'
+    })
+});
+
+router.get('/api/projects' (req, res, next) => {
+
+});
+
+router.get('/api/projects/:id' (req, res, next) => {
+
+});
+
+router.post('/api/projects' (req, res, next) => {
+
+});
+
+router.put('/api/projects/:id' (req, res, next) => {
+
+});
+
+router.delete('/api/projects/:id' (req, res, next) => {
+
+});
