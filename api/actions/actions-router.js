e585bb8bbbdbad3fff92f24fb65d42cd57e10d26
matieuruth <matieu.ruth@gmail.com>
2021-02-21 22:02:57 -0500

    making progress

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,9 @@
 // Write your "actions" router here!
+const express = require('express');
+const actions = require('./actions-model');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+
+});
